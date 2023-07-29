a7fc737125d85d887cb59015cdfdccb5f8ff3c9a
mohammad_rizwan <rizwanm76@yahoo.com>
2023-07-29 14:47:32 -0400

    small changes, use Scanner

--- Calculator.java
+++ Calculator.java
@@ -1,34 +1,53 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Basic Calculator!");
-        System.out.println("Please enter your first number: ");
-        double num1 = Double.parseDouble(System.console().readLine());
+        // Welcome message
+        System.out.println("Welcome to the Advanced Calculator!");
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Please enter your second number: ");
-        double num2 = Double.parseDouble(System.console().readLine());
+        try {
+            // Read the first number from the user
+            System.out.print("Please enter your first number: ");
+            double num1 = Double.parseDouble(scanner.nextLine());
 
-        System.out.print("Please enter the operation you would like to perform: ");
-        String op = System.console().readLine();
+            // Read the second number from the user
+            System.out.print("Please enter your second number: ");
+            double num2 = Double.parseDouble(scanner.nextLine());
 
-        switch (op) {
-            case "+":
-                System.out.println("The result is " + (num1 + num2));
-                break;
-            case "-":
-                System.out.println("The result is " + (num1 - num2));
-                break;
-            case "*":
-                System.out.println("The result is " + (num1 * num2));
-                break;
-            case "/":
-                if (num2 != 0) {
-                    System.out.println("The result is " + (num1 / num2));
-                } else {
-                    System.out.println("Error: Division by zero.");
-                }
-                break;
-            default:
-                System.out.println("Invalid operation entered.");
+            // Read the operation from the user
+            System.out.print("Please enter the operation you would like to perform: ");
+            String op = scanner.nextLine();
+
+            // Switch statement based on the operation selected by the user
+            switch (op) {
+                // Addition
+                case "+":
+                    System.out.println("The result is " + (num1 + num2));
+                    break;
+                // Subtraction
+                case "-":
+                    System.out.println("The result is " + (num1 - num2));
+                    break;
+                // Multiplication
+                case "*":
+                    System.out.println("The result is " + (num1 * num2));
+                    break;
+                // Division
+                case "/":
+                    // Check if the divisor is zero
+                    if (num2 == 0) {
+                        System.out.println("Error: Division by zero.");
+                    } else {
+                        System.out.println("The result is " + (num1 / num2));
+                    }
+                    break;
+                // Invalid operation
+                default:
+                    System.out.println("Invalid operation entered.");
+            }
+        } finally {
+            scanner.close(); // Close the Scanner after using it.
         }
     }
 }
